4aba88ca6ca21ec110d6c89a288ecf080b3a61f4
akash1rakuzan <shchukinmaxym@gmail.com>
2025-08-19 12:56:09 +0300

    backup before DOTween installation
    currently working on stickman instantiation

--- Count Masters/Assets/Scripts/PlayerManager.cs
+++ Count Masters/Assets/Scripts/PlayerManager.cs
@@ -4,17 +4,33 @@ public class PlayerManager : MonoBehaviour
 {
     public Transform player;
     private int numberOfStickmans;
-    [SerializeField] private TextMeshProUGUI CounterTxt;
+    [SerializeField] private TextMeshPro CounterTxt;
     [SerializeField] private GameObject stickMan;
+    //******************************************
 
+    [Range(0f,1f)][SerializeField] private float DistanceFactor, Radius;
     void Start()
     {
+        player = transform;
         numberOfStickmans = transform.childCount - 1;
         CounterTxt.text = numberOfStickmans.ToString();
     }
 
     void Update()
     {
+        
+    }
+
+    private void FormatStickMan()
+    {
+        for (int i = 0; i < player.childCount; i++)
+        {
+            var x = DistanceFactor * Mathf.Sqrt(i) * Mathf.Cos(i * Radius);
+            var z = DistanceFactor * Mathf.Sqrt(i) * Mathf.Sin(i * Radius);
+            var NewPos = new Vector3(x, 0f, z);
+            player.transform.GetChild(i).DOLocalMove(NewPos, 1f).SetEase(Ease.OutBack);
+        }
+    
         
     }
 
@@ -29,7 +45,7 @@ public class PlayerManager : MonoBehaviour
         }
         numberOfStickmans = transform.childCount - 1;
         CounterTxt.text = numberOfStickmans.ToString();
-
+        FormatStickMan();
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -38,8 +54,23 @@ public class PlayerManager : MonoBehaviour
         if (other.CompareTag("gate"))
         {
             other.transform.parent.GetChild(0).GetComponent<BoxCollider>().enabled = false;
+            other.transform.parent.GetChild(1).GetComponent<BoxCollider>().enabled = false;
+
+            var gateManager = other.GetComponent <GateManager>();
+
+            if (gateManager.multiply)
+            {
+                MakeStickMan(numberOfStickmans * gateManager.randomNumber);
+
+
+            }
+            else 
+            {
+                MakeStickMan(numberOfStickmans + gateManager.randomNumber);
+
+            }
             
-          
+
         }
     
     }
